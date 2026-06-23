b4cc1b7bb2b02a00fd0712f422e1886d26d813d9
nmya-m <166662882+nmya-m@users.noreply.github.com>
2026-06-23 18:47:25 +0000

    excerise 1 finsihed

--- src.py
+++ src.py
@@ -1,43 +1,43 @@
 import math
 from random import random
 
-
-state = [1,...,1]
+state = [1] * 100 #list of 100 monomers of length 1
 t = 0
 
 max_time = 5; #placeholder for now
+
+trajectory = [(state.copy(), t)]  # store the initial state and time
 
 #reaction rates
 k_plus = 5 ; #placeholder for now
 k_minus = 7; #placeholder for now
 
-#while t < max_time:
+while t < max_time:
     #PART 1: two reaction types (join or break)
     #join
-        n = len(state)
-        a_join = k_plus * ((n*(n-1))/2)
+    n = len(state)
+    a_join = k_plus * ((n*(n-1))/2)
 
     #break
-        n_bonds = sum(state[i]-1 for i in range(len(state)))
-        a_break = k_minus * n_bonds
+    n_bonds = sum(state[i]-1 for i in range(len(state)))
+    a_break = k_minus * n_bonds
 
-        a_total = a_join + a_break
+    a_total = a_join + a_break
 
-        if a_total == 0: #no more reactions can occur
-          #  break
+    if a_total == 0: #no more reactions can occur
+        break
 
-    #UNFINISHED!!
     #PART 2: determine time to next reaction
 
-T = -math.log(random.random())/a_total
+    r = random.uniform(0,1)
+    T = -math.log(r)/a_total #random time
 
-t = t + T
+    t = t + T
     
-
     #PART 3: determine which reaction occurs
-    f = random(0,1) * a_total
+    f = r * a_total
 
-if f < a_join:
+    if f < a_join:
         #join reaction occurs
         #choose two random indices to join
         i, j = random.sample(range(len(state)), 2)
@@ -46,9 +46,14 @@ if f < a_join:
     else:
         #break reaction occurs
         #choose a random index to break
-        i = random.choice(range(len(state)))
-        if state[i] > 2:
-               bonds = state[i] - 1  # number of bonds to break
-               random_bond = random.randint(1, bonds)  # choose a bond to break
-               state[i] -= random_bond  # break the bond
-               state.append(random_bond)  # add the new fragment to the state
+        bond_index = random.randint(0, n_bonds - 1)
+        for i in range(len(state)):
+            if bond_index < state[i] - 1:
+                #break the bond at index i
+                state[i] -= bond_index
+                state.append(bond_index)  # add a new monomer of length 1
+                break
+            else:
+                bond_index -= (state[i] - 1)
+
+    trajectory.append((state.copy(), t))    
